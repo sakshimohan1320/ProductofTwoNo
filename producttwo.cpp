7e88a28ade9572f94f6fd866baa88ccb55e5b4cb
sakshimohan1320 <66312941+sakshimohan1320@users.noreply.github.com>
2020-07-21 15:39:47 +0530

    Created producttwo.cpp
    This file will find the product of two numbers and display the result on the screen.

---
+++ producttwo.cpp
@@ -0,0 +1,10 @@
+#include <iostream.h>
+int main()
+{
+    double firstNumber, secondNumber, productOfTwoNumbers;
+    cout << "Enter two numbers: ";
+    cin >> firstNumber >> secondNumber;
+ productOfTwoNumbers = firstNumber * secondNumber;  
+ cout << "Product = " << productOfTwoNumbers;    
+ return 0;
+ }
